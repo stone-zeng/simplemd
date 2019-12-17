3e2db3263c8fbb1efa9b4fd7532b1fdb239c6fc6
Xiangdong Zeng <xdzeng96@gmail.com>
2019-12-17 17:28:17 +0800

    multiline pre; add `BlockState`; adjust indent

--- parse.hs
+++ parse.hs
@@ -8,25 +8,41 @@ import Text.Pretty.Simple (pPrint)
 
 main :: IO ()
 main = do
-    pPrint $ foldl parseString "XX" [1, 2, 33, 444]
-    pPrint $ foldl parseMarkdown (Markdown []) [
-            "## Title",
-            "# Title2",
-            "```c",
-            "- help",
-            "1. First",
-            "22. Twenty-two",
-            "1.invalie ol",
-            "-invalid ul",
-            "> quote",
-            ">> invalid quote",
-            ">2invalid quote",
-            "``invalid code"
-        ]
-
-type Result = String
-parseString :: Result -> Int -> Result
-parseString result x = "(" ++ result ++ "+" ++ show x ++ ")"
+  pPrint $ foldl parseMarkdown (Markdown []) [
+      "```c"
+    , "int main () {"
+    , "    return 0;"
+    , "}"
+    , "```"
+    ]
+  pPrint $ foldl parseMarkdown (Markdown []) [
+      "```python"
+    , "def f:"
+    , "\tpass"
+    , "```endpython"
+    , "```"
+    ]
+  pPrint $ foldl parseMarkdown (Markdown []) [
+      "```python"
+    , "def f:"
+    , "\tpass"
+    , "```"
+    , "something after"
+    ]
+  pPrint $ foldl parseMarkdown (Markdown []) [
+      "## Title"
+    , "# Title2"
+    , "```c"
+    , "- help"
+    , "1. First"
+    , "22. Twenty-two"
+    , "1.invalie ol"
+    , "-invalid ul"
+    , "> quote"
+    , ">> invalid quote"
+    , ">2invalid quote"
+    , "``invalid code"
+    ]
 
 --------------------------------------------------------------------------------
 
@@ -37,21 +53,22 @@ data ElemType = Plain
               | Strong    -- ^ HTML <strong>
               | Link Url  -- ^ HTML <a href="...">
               | Math      -- ^ Use MathJax/KaTeX
-    deriving (Show)
+  deriving (Show)
 type Url = String
 
 data InlineElem = InlineElem {
-        elemType    :: ElemType,
-        elemContent :: String
-    } deriving (Show)
+    elemType    :: ElemType,
+    elemContent :: String
+  } deriving (Show)
 
-data BlockElem = BlockPara    Para     -- ^ HTML <p>
-               | BlockHeading Heading  -- ^ HTML <h1>, <h2>, ... <h6>
-               | BlockPre     Pre      -- ^ HTML <pre>
-               | BlockUlist   Ulist    -- ^ HTML <ul>
-               | BlockOlist   Olist    -- ^ HTML <ol>
-               | BlockQuote   Quote    -- ^ HTML <blockquote>
-    deriving (Show)
+data BlockElem = BlockPara    Para    BlockState -- ^ HTML <p>
+               | BlockHeading Heading BlockState -- ^ HTML <h1>, <h2>, ... <h6>
+               | BlockPre     Pre     BlockState -- ^ HTML <pre>
+               | BlockUlist   Ulist   BlockState -- ^ HTML <ul>
+               | BlockOlist   Olist   BlockState -- ^ HTML <ol>
+               | BlockQuote   Quote   BlockState -- ^ HTML <blockquote>
+  deriving (Show)
+data BlockState = Open | Closed deriving (Show)
 
 -- HTML <p>
 newtype Para = Para [InlineElem] deriving (Show)
@@ -64,27 +81,27 @@ data Heading = Heading {
 
 -- HTML <pre>
 data Pre = Pre {
-        preLang    :: String,
-        preContent :: String
-    } deriving (Show)
+    preLang    :: String,
+    preContent :: String
+  } deriving (Show)
 
 -- HTML <ul>
 newtype Ulist = Ulist [ListItem] deriving (Show)
 
 -- HTML <ol>
 data Olist = Olist {
-        olStart   :: Int,
-        olContent :: [ListItem]
-    } deriving (Show)
+    olStart   :: Int,
+    olContent :: [ListItem]
+  } deriving (Show)
 
 -- HTML <li>
 data ListItem = ListInlineItem [InlineElem]
               | ListBlockItem  [ListBlockElem]
-    deriving (Show)
+  deriving (Show)
 data ListBlockElem = ListBlockPara  Para
                    | ListBlockUlist Ulist
                    | ListBlockOlist Olist
-    deriving (Show)
+  deriving (Show)
 
 -- HTML <blockquote>
 newtype Quote = Quote [BlockElem] deriving (Show)
@@ -101,47 +118,58 @@ olistPattern   = mkRegex "^([0-9]+)\\. (.*)"    -- [<number>, <content>]
 quotePattern   = mkRegex "^> (.*)"              -- [<content>]
 
 parseHeading, parsePre, parseUlist, parseOlist, parseQuote :: [String] -> Markdown
-parseHeading result = Markdown [BlockHeading heading]
-    where heading = Heading {
-        headingLevel   = length $ head result,
-        headingContent = parseInline $ last result
-    }
-parsePre result = Markdown [BlockPre pre]
-    where pre = Pre {
-        preLang    = head result,
-        preContent = ""
-    }
-parseUlist result = Markdown [BlockUlist ul]
-    where ul = Ulist [ListInlineItem $ parseInline $ last result]
-parseOlist result = Markdown [BlockOlist ol]
-    where ol = Olist {
-        olStart   = read $ head result,
-        olContent = [ListInlineItem $ parseInline $ last result]
-    }
-parseQuote result = Markdown [BlockQuote quote]
-    where quote = Quote [BlockPara $ Para $ parseInline $ head result]
+parseHeading result = Markdown [BlockHeading heading Closed]
+  where heading = Heading {
+    headingLevel   = length $ head result,
+    headingContent = parseInline $ last result
+  }
+parsePre result = Markdown [BlockPre pre Open]
+  where pre = Pre {
+    preLang    = head result,
+    preContent = ""
+  }
+parseUlist result = Markdown [BlockUlist ul Open]
+  where ul = Ulist [ListInlineItem $ parseInline $ last result]
+parseOlist result = Markdown [BlockOlist ol Open]
+  where ol = Olist {
+    olStart   = read $ head result,
+    olContent = [ListInlineItem $ parseInline $ last result]
+  }
+parseQuote result = Markdown [BlockQuote quote Open]
+  where quote = Quote [BlockPara (Para $ parseInline $ head result) Open]
 
 parseInline :: String -> [InlineElem]
 parseInline s = [InlineElem {elemType = Plain, elemContent = s}]
 
 parseMarkdown :: Markdown -> String -> Markdown
 parseMarkdown (Markdown []) s =
-    case matchRegex headingPattern s of
-        Just result -> parseHeading result
+  case matchRegex headingPattern s of
+    Just result -> parseHeading result
+    Nothing ->
+      case matchRegex prePattern s of
+        Just result -> parsePre result
         Nothing ->
-            case matchRegex prePattern s of
-                Just result -> parsePre result
+          case matchRegex ulistPattern s of
+            Just result -> parseUlist result
+            Nothing ->
+              case matchRegex olistPattern s of
+                Just result -> parseOlist result
                 Nothing ->
-                    case matchRegex ulistPattern s of
-                        Just result -> parseUlist result
-                        Nothing ->
-                            case matchRegex olistPattern s of
-                                Just result -> parseOlist result
-                                Nothing ->
-                                    case matchRegex quotePattern s of
-                                        Just result -> parseQuote result
-                                        Nothing -> Markdown [BlockPara $ Para $ parseInline s]
+                  case matchRegex quotePattern s of
+                    Just result -> parseQuote result
+                    Nothing -> Markdown [BlockPara (Para $ parseInline s) Open]
 
 parseMarkdown (Markdown mdElements) s =
-    Markdown (mdElements ++ newMdElems)
-    where Markdown newMdElems = parseMarkdown (Markdown []) s
+  case last mdElements of
+    BlockPre pre Open ->
+      case s of
+        "```" -> Markdown $ init mdElements ++ [BlockPre pre Closed]
+        _     -> Markdown $ init mdElements ++ [BlockPre newPre Open]
+        where newPre = Pre {
+          preLang    = preLang pre,
+          preContent = preContent pre ++ "\n" ++ s
+        }
+    _ -> undefined
+  -- Markdown (mdElements ++ newMdElems)
+  -- where Markdown newMdElems =
+  -- parseMarkdown (Markdown []) s
--- simplemd.hs
+++ simplemd.hs
@@ -14,33 +14,33 @@ main = startGUI defaultConfig setup
 
 setup :: Window -> UI ()
 setup rootWindow = void $ do
-    return rootWindow # set title "SVG"
+  return rootWindow # set title "SVG"
 
-    heading <- UI.h1 # set text "SVG Two Ways"
+  heading <- UI.h1 # set text "SVG Two Ways"
 
-    textarea <- (UI.div # set contenteditable "true") #+ [
-            UI.div # set text "# Heading 1",
-            UI.div # set text "## Heading 2"
-        ]
-    result <- UI.div # set text ""
+  textarea <- (UI.div # set contenteditable "true") #+ [
+      UI.div # set text "# Heading 1"
+    , UI.div # set text "## Heading 2"
+    ]
+  result <- UI.div # set text ""
 
-    getBody rootWindow #+ [
-            element heading,
-            element textarea,
-            element result
-        ]
+  getBody rootWindow #+ [
+      element heading
+    , element textarea
+    , element result
+    ]
 
-    on UI.valueChange textarea $ \_ -> do
-        markdownText <- get myText textarea
-        -- element textarea # set html (highlight markdownText)
-        element result   # set text (parse markdownText)
+  on UI.valueChange textarea $ \_ -> do
+    markdownText <- get myText textarea
+    -- element textarea # set html (highlight markdownText)
+    element result   # set text (parse markdownText)
 
 contenteditable :: WriteAttr Element String
 contenteditable = mkWriteAttr (set' (attr "contenteditable"))
 
 myText :: Attr Element String
 myText = mkReadWriteAttr _get _set
-    where
+  where
     _get   el = callFunction $ ffi "$(%1).html()" el
     _set v el = runFunction  $ ffi "$(%1).html(%2)" el v
 
@@ -55,12 +55,12 @@ parse htmlStr = htmlStr ++ "  ......  " ++ (intercalate " + " $ map (\x -> "\"" 
 
 splitLine :: String -> [String]
 splitLine s = removeEmptyLine $ map replaceBr $ splitRegex pattern s
-    where
-        pattern = mkRegex "</div><div>|<div>|</div>"
+  where
+    pattern = mkRegex "</div><div>|<div>|</div>"
 
-        replaceBr "<br>" = ""
-        replaceBr x = x
+    replaceBr "<br>" = ""
+    replaceBr x = x
 
-        removeEmptyLine [] = []
-        removeEmptyLine ("":xs) = removeEmptyLine xs
-        removeEmptyLine x = if (last x == "") then removeEmptyLine (init x) else x
+    removeEmptyLine [] = []
+    removeEmptyLine ("":xs) = removeEmptyLine xs
+    removeEmptyLine x = if (last x == "") then removeEmptyLine (init x) else x
